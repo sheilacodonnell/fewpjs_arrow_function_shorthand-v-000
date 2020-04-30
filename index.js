1597e75504f56171f0950bf54e6bb775089794f2
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-30 13:59:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 let divide = (parameter1, parameter2) => {
-  return parameter1 + parameter2;
+  return parameter1 / parameter2;
 }
 
 let square = (parameter1) => {
   return parameter1 * parameter1;
+}
+
+let add = (parameter1, parameter2) => {
+  return parameter1 + parameter2;
 }
