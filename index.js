1e380dc5fb53801f4438cd51ec42223a87b43d97
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-30 14:11:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+let divide = () => {
+  return 2000 / 100;
+}
+
+let square = (parameter1) => {
+  return parameter1 * parameter1;
+}
+
+let add = (parameter1, parameter2) => {
+  return parameter1 + parameter2;
+}
